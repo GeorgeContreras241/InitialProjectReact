7c975c1429616f43113e2fb78b5793ec5724e348
GeorgeContreras241 <maicolcontreras241@gmail.com>
2024-05-15 21:01:01 -0500

    End

--- InitialProject/src/components/Title.jsx
+++ InitialProject/src/components/Title.jsx
@@ -1,7 +1,25 @@
+import { useState } from "react"
 
 
 export const Title = () => {
+    const [input, setInput] = useState('')
+    const [update, setUpdate] = useState('')
+    const handleInput = (event) => {
+      setInput(event.target.value)
+    }
+    const onClick = (event) =>{
+      event.preventDefault()
+      setUpdate(input)
+    }
   return (
-    <div>Componente</div>
+    <div>
+        <input 
+        type="text" 
+        value={input}
+        onChange={handleInput}/>
+        <button onClick={onClick}>Cambiar</button>
+        <p>texto del input : <strong>{input}</strong></p> 
+        <p>texto enviado por el boton: <strong>{update}</strong></p>
+    </div>
   )
 }
